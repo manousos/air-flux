91fb8801ed70dff5805993f5950a2ddc8ec60f49
manousos <man.mathioudakis@gmail.com>
2018-12-08 19:38:13 +0200

    add java doc

--- src/main/java/gr/manousos/air/config/AppConfig.java
+++ src/main/java/gr/manousos/air/config/AppConfig.java
@@ -6,10 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+/**
+ * Application configuration.
+ */
 @Configuration
 @EnableMongoRepositories(basePackages = "gr.manousos.air.repository")
 public class AppConfig {
 
+  /**
+   * Bean to map IATA code with airport city name.
+   *
+   * @return a map with key the IATA code and value the city name.
+   */
   @Bean
   Map<String, String> airportToCityMap() {
     Map<String, String> airportMap = new HashMap<>();
