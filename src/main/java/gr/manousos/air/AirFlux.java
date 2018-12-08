0925c68ae7b6d388d810e1345a1241c7dae35f81
manousos <man.mathioudakis@gmail.com>
2018-12-08 19:34:59 +0200

    Initial commit

---
+++ src/main/java/gr/manousos/air/AirFlux.java
@@ -0,0 +1,12 @@
+package gr.manousos.air;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFlux {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AirFlux.class, args);
+  }
+}
